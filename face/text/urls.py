a1c9ee7356e995820fb46dbb7a3485b5b6640c55
Bala prajapatil <balapp143@gmail.com>
2021-03-15 20:00:49 +0530

    changes

--- face/text/urls.py
+++ face/text/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.conf.urls import url
-from text.views import imagetotext,success,delete_text,typing_test
+from text.views import imagetotext,success,delete_text,typing_test,login_,success_login,logout_view
  
 from django.conf.urls.static import static
 from django.conf import  settings
@@ -8,6 +8,9 @@ from django.conf import  settings
 
 urlpatterns = [
     
+	url(r'^login/$', login_, name='login'),
+	url(r'^logout_view/$', logout_view, name='logout_view'),
+	url(r'^success_login/$', success_login, name='success_login'),
 	url(r'^imagetotext/$', imagetotext, name='imagetotext'),
 	url(r'^success/$', success, name='success'),
 	url(r'^delete_text/(?P<id>\d+)$', delete_text, name='delete_text'),
--- face/text/views.py
+++ face/text/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import ImageToText
 from text.forms import ImageToTextForm
 from rest_framework.response import Response
 from face2.models import Face
+from django.urls import reverse
 
 from docx import Document
 from PIL import Image
@@ -14,6 +15,8 @@ from curses import ascii
 from PIL import Image
 from django.shortcuts import get_list_or_404, get_object_or_404
 from django.views.generic import TemplateView
+from playsound import playsound
+from django.contrib.auth.models import User
 
 all_face = Face.objects.all().count()
 
@@ -106,3 +109,77 @@ def typing_test(request, template_name='time_speed.html'):
     time = 'time'
 
     return render(request, template_name, locals())
+
+
+
+def play_song(request):
+    play = playsound('/path/to/a/sound/file/you/want/to/play.mp3')
+    return play
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+
+from django.contrib.auth import login as auth_login
+def login_(request):
+    count_all_face = Face.objects.all().count()
+    user_name = request.POST.get('uname')
+    user_pass = request.POST.get('psw')
+    user = authenticate(username=user_name, password=user_pass)
+    print(user,"invalid dataaaaaaaaa")
+    if user is not None:
+        login(request, user)
+        if user.is_superuser:
+            # print(request.user.is_authenticated)
+            # print(request.user)
+            # print(user.is_superuser,'is_superuser')
+            success_url = reverse('success_login')
+            return HttpResponseRedirect(success_url)
+        else:
+            print('nonnnnnnnnnnn')
+    else:
+        validation_error = 'Please Enter correct username and password'
+        return render(request, locals())
+
+
+    # if user.is_superuser:
+    #     print(user.password)
+    #     success_url = reverse('success_login')
+    #     print(success_url,"jjjjjjjjjjjjjjj")
+    #     return HttpResponseRedirect(success_url)
+    #     # A backend authenticated the credentials
+    # else:
+    #     print('nonnnnnnn')
+    # No backend authenticated the credentials
+
+    # user = User.objects.filter(username=user_name,password=user_pass)
+    # print(user,"kkkkkkkkkkkkkkkkkkkkk")
+    # if user.is_superuser:
+    #     print('super user')
+    # else:
+    #     print('user is not found')
+    return render(request, locals())
+
+
+
+from django.contrib.auth import get_user_model
+def success_login(request, template_name="super_admin_dashboad.html"):
+    count_all_face = Face.objects.all().count()
+    bala = 'login'
+    User = get_user_model()
+    print(User.username)
+    print(request.user.get_username())
+    print(request.user.username,"kkkkkk")
+    print(request.user.is_authenticated, request.user.is_superuser)
+
+    return render(request,template_name, locals())
+
+
+
+
+
+def logout_view(request):
+    count_all_face = Face.objects.all().count()
+    logout(request)
+    success_url = reverse('home')
+    return HttpResponseRedirect(success_url)
+    # Redirect to a success page.
